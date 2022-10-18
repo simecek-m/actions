bb02c585928e6b91f6f692cc2bd5410053503591
Martin Šimeček <simecek.m@seznam.cz>
2022-10-18 21:01:26 +0200

    refactoring project

---
+++ src/server/index.ts
@@ -0,0 +1,19 @@
+import { Application } from "express";
+import { createServer, Server } from "http";
+import logger from "logger";
+
+export async function start(app: Application): Promise<void> {
+  logger.info("Starting HTTP server.");
+  const server: Server = createServer(app);
+  function promisifyStart(): Promise<void> {
+    return new Promise((res, rej) => {
+      server
+        .on("listening", res)
+        .on("error", rej)
+        .listen(process.env.EXPRESS_SERVER_PORT);
+    });
+  }
+  return promisifyStart();
+}
+
+export default { start };
